61e058cc47a571517ba5d1321542929c4c63a7f0
nlaz <nikolazaris@utexas.edu>
2018-06-21 21:44:35 -0400

    Fix client side routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Categories from "./Categories";
 import Decks from "./Decks";
@@ -11,13 +11,13 @@ class App extends Component {
   render() {
     return (
       <div className="mt-5">
-        <BrowserRouter>
+        <HashRouter>
           <Switch>
             <Route exact path="/" component={Categories} />
             <Route exact path="/:categoryId" component={Decks} />
             <Route exact path="/decks/:deckId" component={Review} />
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   }
