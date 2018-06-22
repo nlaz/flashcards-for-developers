bcc14b600fb6b45f8c048ab84f8d832a2a67cbe2
nlaz <nikolazaris@utexas.edu>
2018-06-22 15:47:39 -0400

    Update review experience

--- src/app/Review.js
+++ src/app/Review.js
@@ -1,17 +1,57 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import cx from "classnames";
+import Chance from "chance";
+import injectSheet from "react-jss";
 
 import Octicon from "../components/Octicon";
-
 import * as api from "./apiActions";
+import "./Review.css";
+
+const chance = new Chance();
+
+const styles = {
+  progressBar: {
+    height: 10,
+    borderRadius: "999px",
+    background: "#eeeeee",
+  },
+  progress: {
+    height: 10,
+    borderRadius: "999px",
+  },
+};
+
+const ProgressBar = injectSheet(styles)(({ classes, index, length }) => (
+  <div className={cx(classes.progressBar, "w-100 mb-3")}>
+    <div
+      className={cx(classes.progress, "bg-dark")}
+      style={{ width: 100 * (index + 1) / length + "%" }}
+    />
+  </div>
+));
 
 class Review extends Component {
-  state = { deck: {}, cards: [] };
+  state = { deck: {}, cards: [], options: [], index: 0, isWrong: false };
 
   componentWillMount() {
     const { params } = this.props.match;
     this.fetchDeck(params.deckId);
   }
+
+  onClick = (answer, card) => {
+    if (answer.id === card.id) {
+      const { cards } = this.state;
+      const index = Math.min(this.state.index + 1, cards.length - 1);
+      this.setState({ index, options: this.getOptions(index, cards) });
+    } else {
+      this.setState({ isWrong: true }, () =>
+        setTimeout(() => this.setState({ isWrong: false }), 1000),
+      );
+    }
+  };
+
+  checkAnswer = (answer, card) => {};
 
   fetchDeck = deckId => {
     api.fetchDeck(deckId).then(response => {
@@ -20,13 +60,33 @@ class Review extends Component {
   };
 
   fetchCards = deck => {
+    const { index } = this.state;
     api.fetchCards(deck).then(response => {
-      this.setState({ cards: response });
+      const cards = chance.shuffle(response);
+      this.setState({ cards, options: this.getOptions(index, cards) });
     });
   };
 
+  onToggle = () => this.setState(({ toggle }) => ({ toggle: !toggle }));
+
+  getOptions = (index, cards) => {
+    const random = chance.unique(chance.natural, 3, { min: 0, max: cards.length - 1 });
+    const opts = chance.shuffle([...random, index]);
+    return opts.map(el => cards[el]);
+  };
+
   render() {
-    const { deck, cards } = this.state;
+    const { deck, cards, options, index } = this.state;
+
+    if (cards.length === 0) {
+      return (
+        <div className="container p-4">
+          <h1 className="text-secondary">Loading cards...</h1>
+        </div>
+      );
+    }
+
+    const selected = cards[index];
 
     return (
       <div className="container p-4">
@@ -38,15 +98,39 @@ class Review extends Component {
           <h1 className="m-0">{deck.name}</h1>
           {deck.description && <p>{deck.description}</p>}
         </div>
-        <div className="row mt-5 pt-5">
-          {cards.map((card, key) => (
-            <div className="col-12 border border-dark rounded mb-4 py-5" key={key}>
-              <div
-                className="text-center bg-white m-3 p-5"
-                dangerouslySetInnerHTML={{ __html: card.front }}
-              />
+        <div className="row mt-5 pt-5 px-3">
+          <ProgressBar index={index} length={cards.length} />
+          <div
+            className={cx("col-12 border border-dark rounded mb-4 py-5", {
+              shake: this.state.isWrong,
+            })}
+          >
+            <div className="row px-4">
+              <div className="col-6">
+                <div
+                  className="text-center border rounded bg-white p-5 h-100 d-flex align-items-center justify-content-center"
+                  dangerouslySetInnerHTML={{ __html: selected.front }}
+                />
+              </div>
+              <div className="col-6">
+                {options.map((option, key) => (
+                  <div
+                    key={key}
+                    onClick={() => this.onClick(option, selected)}
+                    className={cx("border rounded d-flex align-items-center p-3", {
+                      "mb-2": options.length !== key + 1,
+                    })}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div className="border rounded mr-3 px-2" style={{ fontSize: ".9em" }}>
+                      {key + 1}
+                    </div>
+                    {option.back}
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
     );
