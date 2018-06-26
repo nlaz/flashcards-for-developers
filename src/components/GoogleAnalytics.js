edce0b582d02335068db10c89e04ac3fe7e6aadf
nlaz <nikolazaris@utexas.edu>
2018-06-26 14:42:10 -0400

    Update analytics component

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -4,18 +4,10 @@ import config from "../config";
 
 ReactGA.initialize(config.googleAnalyticsKey);
 
-export function logSignupEvent(userId) {
-  ReactGA.event({
-    category: "User",
-    action: "created",
-  });
-}
-
 class GoogleAnalytics extends Component {
   render() {
-    console.log("ga key", config.googleAnalyticsKey);
-    ReactGA.set({ page: window.location.pathname + window.location.search });
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.set({ page: window.location.pathname + window.location.hash });
+    ReactGA.pageview(window.location.pathname + window.location.hash);
     return null;
   }
 }
