565201f89a642bb50123958eb14f8b33e02c677d
nlaz <nikolazaris@utexas.edu>
2018-08-27 19:55:04 -0400

    Add tooltip

--- src/app/review/StudyToggle.js
+++ src/app/review/StudyToggle.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Tooltip from "rc-tooltip";
+import "rc-tooltip/assets/bootstrap.css";
 
 import * as preferences from "../utils/prefs";
 import Toggle from "../../components/Toggle";
@@ -19,19 +21,26 @@ class StudyToggle extends React.Component {
 
   render() {
     const { isChecked } = this.state;
+
+    const tooltip = (
+      <span>Hide cards you've seen recently so you only study your weakest cards.</span>
+    );
+
     return (
       <div className="d-flex align-items-center">
         <Toggle onChange={this.onChange} checked={isChecked} />
-        <small className="ml-2" style={{ opacity: 0.4 }}>
-          Hide familiar cards{" "}
-          <span
-            className="fa-stack fa-xs"
-            style={{ lineHeight: "1.7em", fontSize: ".65em", opacity: 0.6 }}
-          >
-            <i className="fas fa-circle fa-stack-2x" />
-            <i className="fas fa-info fa-stack-1x fa-inverse" />
-          </span>
-        </small>
+        <Tooltip placement="bottom" trigger={["click", "hover", "focus"]} overlay={tooltip}>
+          <small className="ml-2" style={{ opacity: 0.4 }}>
+            Hide familiar cards{" "}
+            <span
+              className="fa-stack fa-xs"
+              style={{ lineHeight: "1.7em", fontSize: ".65em", opacity: 0.6 }}
+            >
+              <i className="fas fa-circle fa-stack-2x" />
+              <i className="fas fa-info fa-stack-1x fa-inverse" />
+            </span>
+          </small>
+        </Tooltip>
       </div>
     );
   }
