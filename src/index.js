0364b9aaa8ca4829c6a6efc83499a9b3bc754486
nlaz <nikolazaris@utexas.edu>
2018-08-28 18:29:00 -0400

    Unregister service workers

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app/App";
-import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 import "primer-markdown/build/build.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+unregister();
