d7303f111239b77b30c56a751c49f90a8baa22fb
nlaz <nikolazaris@utexas.edu>
2018-09-07 19:02:53 -0400

    Add cards endpoint

--- server/routes.js
+++ server/routes.js
@@ -3,13 +3,17 @@ const express = require("express");
 const isAuthenticated = require("./middleware/isAuthenticated");
 const UsersController = require("./controllers/UsersController");
 const CardsController = require("./controllers/CardsController");
+<<<<<<< HEAD
 const DecksController = require("./controllers/DecksController");
 const CollectionsController = require("./controllers/CollectionsController");
+=======
+>>>>>>> Add cards endpoint
 
 const router = express.Router();
 
 router.get("/hello", (req, res) => res.send({ message: "Hello world!" }));
 
+<<<<<<< HEAD
 router.get("/api/decks", DecksController.getDecks);
 
 router.get("/api/decks/:deckId", DecksController.getDeck);
@@ -20,6 +24,12 @@ router.get("/api/cards", CardsController.getCards);
 
 router.post("/auth/github", UsersController.githubUser);
 
+=======
+router.get("/api/cards", CardsController.getCards);
+
+router.post("/auth/github", UsersController.githubUser);
+
+>>>>>>> Add cards endpoint
 router.put("/users/saved_decks", isAuthenticated, UsersController.setSavedDecks);
 
 router.get("/users/saved_decks", isAuthenticated, UsersController.getSavedDecks);
