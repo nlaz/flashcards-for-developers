057c7d9ac1028a409816fc144807ed4b55cd628e
nlaz <nikolazaris@utexas.edu>
2018-09-07 19:45:56 -0400

    Remove dependency on airtable

--- src/app/apiActions.js
+++ src/app/apiActions.js
@@ -1,41 +1,10 @@
-import Airtable from "airtable";
 import axios from "axios";
 import cookie from "js-cookie";
 import queryString from "query-string";
 
-import config from "../config/index";
-
-const base = new Airtable({ apiKey: config.airtableApiKey }).base(config.airtableApiId);
-
 export const fetchCollection = id => {
   const config = { headers: { Authorization: cookie.get("token") } };
   return axios.get(`/api/collections/${id}`, config);
-};
-
-const getDeckFromRecord = record => ({
-  id: record.id,
-  name: record.get("Name"),
-  cards: record.get("Cards"),
-  description: record.get("Description"),
-  category: record.get("Category") || [],
-  type: record.get("Type"),
-  source: record.get("Source"),
-  difficulty: record.get("Difficulty"),
-  stars: record.get("Stars"),
-  createdTime: record.get("Created time"),
-  upvotes: record.get("Upvotes"),
-  downvotes: record.get("Downvotes"),
-  new: record.get("New") || false,
-});
-
-export const updateDeck = async (deckId, body) => {
-  return new Promise((success, failure) => {
-    base("Decks").update(deckId, body, function(err, record) {
-      if (err) failure(err);
-
-      success(getDeckFromRecord(record));
-    });
-  });
 };
 
 export const fetchDecks = collectionId => {
