1a81ef0157115e5df55dbd75d59ef72df428e024
nlaz <nikolazaris@utexas.edu>
2018-09-07 20:54:54 -0400

    Fix skill progress

--- server/models/Deck.js
+++ server/models/Deck.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const DeckSchema = new mongoose.Schema(
   {
     airtableId: { type: String, index: true },
+    cards: [{ type: mongoose.Schema.Types.ObjectId, ref: "Card" }],
     name: String,
     type: String,
     source: String,
     description: String,
-    cards: [String],
     difficulty: [String],
     stars: Number,
     createdTime: String,
