a5bed1d9b9128c58a4c702baeb0abc11708e7783
nlaz <nikolazaris@utexas.edu>
2018-09-10 17:05:10 -0400

    Fix localstorage util method

--- src/app/utils/savedDecks.js
+++ src/app/utils/savedDecks.js
@@ -1,19 +1,19 @@
 const SAVED_DECKS_KEY = "savedDecks";
 
-export const toggleSavedDeck = (deckId, isSaved) => {
-  const decks = this.getSavedDecks();
-
-  const newDecks = isSaved ? decks.filter(el => el !== deckId) : [...decks, deckId];
-
-  this.setSavedDecks(newDecks);
-
-  return this.getSavedDecks();
-};
-
 export const setSavedDecks = decks => {
   localStorage.setItem(SAVED_DECKS_KEY, JSON.stringify(decks));
 };
 
 export const getSavedDecks = () => {
   return JSON.parse(localStorage.getItem(SAVED_DECKS_KEY)) || [];
+};
+
+export const toggleSavedDeck = (deckId, isSaved) => {
+  const decks = getSavedDecks();
+
+  const newDecks = isSaved ? decks.filter(el => el !== deckId) : [...decks, deckId];
+
+  setSavedDecks(newDecks);
+
+  return getSavedDecks();
 };
