5d38d9303e7810536b8b072e9bc15aa55fc188d8
nlaz <nikolazaris@utexas.edu>
2018-09-21 18:39:15 -0400

    Remove comment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-// const sslRedirect = require("heroku-ssl-redirect");
 const forceDomain = require("forcedomain");
 
 const routes = require("./routes");
