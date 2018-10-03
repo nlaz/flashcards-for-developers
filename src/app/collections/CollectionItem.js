1faf0b87eb38e1eaa4b5afd7a30d2f2d3dc04654
nlaz <nikolazaris@utexas.edu>
2018-10-03 17:52:25 -0400

    Fix spacing

--- src/app/collections/CollectionItem.js
+++ src/app/collections/CollectionItem.js
@@ -8,7 +8,7 @@ const CollectionItem = ({ collection }) => (
       to={`/collections/${collection.id}`}
     >
       <div className="collection-image mb-2" style={{ background: collection.color }}>
-        {collection.emoji}
+        <span style={{ width: "16px" }}>{collection.emoji}</span>
       </div>
       <h6 className="font-weight-medium text-dark m-1">{collection.name}</h6>
       <span className="text-secondary">{collection.description}</span>
