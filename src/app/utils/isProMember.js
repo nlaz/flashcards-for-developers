65dc123e8be8d553b9aa8c90e4ac2f4425c4795c
Nick Engmann <cyengmann@gmail.com>
2018-10-19 01:09:13 -0700

    Fixed error with unauthenticated users

--- src/app/utils/isProMember.js
+++ src/app/utils/isProMember.js
@@ -1,7 +1,14 @@
 import cookie from "js-cookie";
 
 const isProMember = () => {
-  const user = JSON.parse(cookie.get("user")) || {};
+  let user;
+
+  try {
+    user = JSON.parse(cookie.get("user")) || {};
+  } catch (error) {
+    // user isn't authenticated
+    return false;
+  }
 
   return Object.keys(user).length > 0 && user.user_plan === "pro_monthly";
 };
