d9df2008f24ccf479b4972b973127402e63a25e2
Niko Lazaris <nikolazaris@gmail.com>
2018-10-22 14:44:58 -0400

    Add chat element (#71)
    * Add chat option
    
    * Restrict chat bubble to /pages
    
    * Remove unneeded env var

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import cookie from "js-cookie";
 
 import About from "./About";
 import PrivacyPolicy from "./PrivacyPolicy";
@@ -7,25 +8,46 @@ import TermsOfService from "./TermsOfService";
 import Membership from "./Membership";
 import MembershipNew from "./MembershipNew";
 import NotFound from "../components/NotFound";
+import isAuthenticated from "../app/utils/isAuthenticated";
 
 import "./Pages.css";
 
-const Pages = () => (
-  <div className="pages">
-    <div className="pages-header">
-      <span className="pages-hero" role="img" aria-label="Tada!">
-        🎉
-      </span>
-    </div>
-    <Switch>
-      <Route path="/pages/about" component={About} />
-      <Route path="/pages/privacy-policy" component={PrivacyPolicy} />
-      <Route path="/pages/terms-of-service" component={TermsOfService} />
-      <Route exact path="/pages/membership" component={Membership} />
-      <Route exact path="/pages/membership/new" component={MembershipNew} />
-      <Route exact path="*" component={NotFound} />
-    </Switch>
-  </div>
-);
+const CHAT_APP_ID = "uhq7cari";
+
+class Pages extends Component {
+  componentDidMount() {
+    const user = isAuthenticated() ? JSON.parse(cookie.get("user")) : {};
+    window.Intercom("boot", {
+      app_id: CHAT_APP_ID,
+      name: user.name,
+      email: user.email,
+      user_plan: user.user_plan,
+    });
+  }
+
+  componentWillUnmount() {
+    window.Intercom("shutdown");
+  }
+
+  render() {
+    return (
+      <div className="pages">
+        <div className="pages-header">
+          <span className="pages-hero" role="img" aria-label="Tada!">
+            🎉
+          </span>
+        </div>
+        <Switch>
+          <Route path="/pages/about" component={About} />
+          <Route path="/pages/privacy-policy" component={PrivacyPolicy} />
+          <Route path="/pages/terms-of-service" component={TermsOfService} />
+          <Route exact path="/pages/membership" component={Membership} />
+          <Route exact path="/pages/membership/new" component={MembershipNew} />
+          <Route exact path="*" component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default Pages;
