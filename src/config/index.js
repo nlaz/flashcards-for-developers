51cdf42f7c4dcf2c58ebd115e2e0e7f38c9d3891
Asad Dhamani <dhamaniasad@gmail.com>
2019-01-18 16:54:57 +0530

    Fix errors for missing Stripe and Mixpanel tokens
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@ const configs = {
   airtableFeedbackUrl: process.env.REACT_APP_AIRTABLE_FEEDBACK_URL || "",
   airtableSuggestionsUrl: process.env.REACT_APP_AIRTABLE_SUGGESTIONS_URL || "",
   airtableEmailUrl: process.env.REACT_APP_AIRTABLE_EMAIL_URL || "",
-  mixpanelAnalyticsKey: process.env.REACT_APP_MIXPANEL_ANALYTICS_KEY || "",
+  mixpanelAnalyticsKey: process.env.REACT_APP_MIXPANEL_ANALYTICS_KEY || "DUMMY_MIXPANEL_TOKEN",
   googleAnalyticsKey: process.env.REACT_APP_GOOGLE_ANALYTICS_KEY || "",
   mailchimpUrl: process.env.REACT_APP_AIRTABLE_MAILCHIMP_URL || "",
   buyMeACoffeeDonateUrl: process.env.REACT_APP_DONATE_URL || "",
   githubOAuthClientId: process.env.REACT_APP_GITHUB_OAUTH_CLIENT_ID || "",
   githubOAuthRedirectURI: process.env.REACT_APP_GITHUB_OAUTH_REDIRECT_URI || "",
-  stripePublicKey: process.env.REACT_APP_STRIPE_PUBLIC_KEY || "",
+  stripePublicKey: process.env.REACT_APP_STRIPE_PUBLIC_KEY || "tok_visa", // Fallback to dummy stripe token
 };
 
 export default configs;
