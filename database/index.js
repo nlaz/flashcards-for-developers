0bbb4c59f3ccde4b4c3a870556097d86f23ad0e1
Asad Dhamani <dhamaniasad@gmail.com>
2019-01-18 16:28:43 +0530

    Warn on missing DATABASE_URI environment variable
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- database/index.js
+++ database/index.js
@@ -3,6 +3,12 @@ const config = require("../config/index");
 
 module.exports = () => {
   mongoose.Promise = global.Promise;
+
+  if (!config.database.uri) {
+  	console.warn("DATABASE_URI environment variable not set.\nPlease set DATABASE_URI environment variable to run flashcards-for-developers");
+  	process.exit();
+  }
+
   mongoose.connect(
     config.database.uri,
     { useNewUrlParser: true },
